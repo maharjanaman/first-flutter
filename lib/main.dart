abfa1f0c8bb533764db6d346366bb416841bc45d
Aman Maharjan <mhrjn.aman@gmail.com>
2020-07-13 14:49:14 +0545

    Change StatelessWidget to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyFlutterAcademind());
 
-class MyFlutterAcademind extends StatelessWidget {
+class MyFlutterAcademind extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyFlutterAcademindState();
+  }
+}
+
+class MyFlutterAcademindState extends State<MyFlutterAcademind> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter Academind'),
         ),
-        body: Text('Hello Flutter'),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            RaisedButton(
+              child: Text('This is question 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('This is question 2'),
+              onPressed: () => print('Answer Two'),
+            ),
+            RaisedButton(
+              child: Text('This is question 3'),
+              onPressed: () {
+                print('Answer Three');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
